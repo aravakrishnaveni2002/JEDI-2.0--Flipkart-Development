0836067ea6bc0aa599c8ffdea6e81099964dd05e
jassi1110 <jasnoorsingh11@gmail.com>
2023-11-23 10:48:42 +0530

    Slot and Seats business logic  : jasnoor

--- GMS_JEDI_Flipkart_Java_POS/src/com/flipkart/business/SeatAllocation.java
+++ GMS_JEDI_Flipkart_Java_POS/src/com/flipkart/business/SeatAllocation.java
@@ -7,5 +7,8 @@ package com.flipkart.business;
  * 
  */
 public class SeatAllocation {
-
+    public static void SeatDetails(){
+        System.out.println("listing all the seat details");
+        return;
+    }
 }
