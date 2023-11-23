5ae8792fccc001f495018d63467d5cb85ccb309e
aravakrishnaveni2002 <aravakrishnaveni2002@gmail.com>
2023-11-23 12:24:22 +0530

    added payment business

---
+++ GMS_JEDI_Flipkart_Java_POS/src/com/flipkart/business/PaymentBusiness.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.flipkart.business;
+
+/**
+ * 
+ */
+public class PaymentBusiness {
+	
+	public static boolean makePayment() {
+		System.out.println("Payment is Successful");
+		return true;
+	}
+}
