4db3b46d2b4fcbc7a3e484c2bf82be4b87ad0456
jassi1110 <jasnoorsingh11@gmail.com>
2023-11-24 14:30:38 +0530

    Adding user to table : Jasnoor

--- GMS_JEDI_Flipkart_Java_POS/src/com/flipkart/business/UserBusiness.java
+++ GMS_JEDI_Flipkart_Java_POS/src/com/flipkart/business/UserBusiness.java
@@ -6,11 +6,36 @@ import com.flipkart.bean.Customer;
 import com.flipkart.bean.GymOwner;
 import com.flipkart.bean.User;
 
+import java.util.*;
+
 /**
  * 
  */
 public class UserBusiness {
 
+	List<User> userList = new ArrayList<>();
+
+	public UserBusiness(){
+		User u1 = new User();
+		u1.setEmail("asmi@flipfit");
+		u1.setRoleId(1);
+		u1.setPassword("12345");
+
+		User u2 = new User();
+		u2.setEmail("arav@flipfit");
+		u2.setRoleId(2);
+		u2.setPassword("Hello");
+
+		User u3 = new User();
+		u3.setEmail("harsha@flipfit");
+		u3.setRoleId(3);
+		u3.setPassword("1234567");
+
+		userList.add(u1);
+		userList.add(u2);
+		userList.add(u3);
+
+	}
 	public void registerCustomer(Customer customer) {
 		System.out.println("Customer registered");
 	}
