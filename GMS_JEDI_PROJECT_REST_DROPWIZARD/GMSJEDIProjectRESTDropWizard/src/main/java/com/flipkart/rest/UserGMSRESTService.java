dfd1d80f2b7cb8e14d3b2817719489fc7e2a77f8
harsha-ag-19 <harshaagrawal2001@gmail.com>
2023-11-28 21:58:18 +0530

    Implemented PostAPI: Harsha

--- GMS_JEDI_PROJECT_REST_DROPWIZARD/GMSJEDIProjectRESTDropWizard/src/main/java/com/flipkart/rest/UserGMSRESTService.java
+++ GMS_JEDI_PROJECT_REST_DROPWIZARD/GMSJEDIProjectRESTDropWizard/src/main/java/com/flipkart/rest/UserGMSRESTService.java
@@ -1,4 +1,22 @@
 package com.flipkart.rest;
 
-public class UserGMSRESTService {
+@Path("/v1/user")
+public class UserGMSRESTService
+{
+    @Path("registerCustomer")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public static void registerCustomer(Customer customer){
+        UserBusinessInterface userBusiness = new UserBusiness();
+        User user =new User();
+        user.setEmail(customer.getEmail());
+        user.setPassword(customer.getPassword());
+        user.setRoleId(3);
+        try {
+            userBusiness.registerUser(user);
+            userBusiness.registerCustomer(customer);
+        } catch (Exception e) {
+
+        }
+    }
 }
