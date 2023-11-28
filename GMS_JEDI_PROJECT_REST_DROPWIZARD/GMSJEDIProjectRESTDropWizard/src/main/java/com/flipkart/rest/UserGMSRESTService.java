0dcefe434ee0066e97500b921dfc1c2d659232a8
jassi1110 <jasnoorsingh11@gmail.com>
2023-11-28 23:05:42 +0530

    Add POST Request to register Customer : Jasnoor

--- GMS_JEDI_PROJECT_REST_DROPWIZARD/GMSJEDIProjectRESTDropWizard/src/main/java/com/flipkart/rest/UserGMSRESTService.java
+++ GMS_JEDI_PROJECT_REST_DROPWIZARD/GMSJEDIProjectRESTDropWizard/src/main/java/com/flipkart/rest/UserGMSRESTService.java
@@ -1,22 +1,24 @@
 package com.flipkart.rest;
 
 @Path("/v1/user")
+@Produces(MediaType.APPLICATION_JSON)
 public class UserGMSRESTService
 {
     @Path("registerCustomer")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public static void registerCustomer(Customer customer){
+    public static Response registerCustomer(Customer customer){
         UserBusinessInterface userBusiness = new UserBusiness();
-        User user =new User();
-        user.setEmail(customer.getEmail());
-        user.setPassword(customer.getPassword());
-        user.setRoleId(3);
+//        User user =new User();
+//        user.setEmail(customer.getEmail());
+//        user.setPassword(customer.getPassword());
+//        user.setRoleId(3);
         try {
-            userBusiness.registerUser(user);
+//            userBusiness.registerUser(user);
             userBusiness.registerCustomer(customer);
         } catch (Exception e) {
-
+            return Response.status(Response.Status.UNAUTHORIZED).entity(exception.getMessage()).build();
         }
+        return Response.ok().build();
     }
 }
