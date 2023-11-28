46ce21f3c332107e252023a38da8fa20ca691162
asmikush111 <asmikush11@gmail.com>
2023-11-28 23:31:26 +0530

    Admin RestAPI added:asmita

--- GMS_JEDI_PROJECT_REST_DROPWIZARD/GMSJEDIProjectRESTDropWizard/src/main/java/com/flipkart/rest/AdminGMSRESTService.java
+++ GMS_JEDI_PROJECT_REST_DROPWIZARD/GMSJEDIProjectRESTDropWizard/src/main/java/com/flipkart/rest/AdminGMSRESTService.java
@@ -4,7 +4,8 @@ import com.flipkart.bean.*;
 import com.flipkart.business.*;
 
 import java.util.List;
-
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -17,11 +18,74 @@ public class AdminGMSRESTService {
     @GET
     @Produces("application/json")
     public static Response viewAllApprovedGymOwners() {
-    	AdminBusinessInterface adminBusiness = new AdminBusiness();
+        AdminBusinessInterface adminBusiness = new AdminBusiness();
         try{
             return Response.ok(adminBusiness.viewAllApprovedGymOnwers()).build();
         }catch(Exception exception){
             return Response.status(Response.Status.UNAUTHORIZED).entity(exception.getMessage()).build();
         }
     }
+
+    @Path("allPendingGymRequests")
+    @GET
+    @Produces("application/json")
+    public static Response viewPendingGymRequests() {
+        AdminBusinessInterface adminBusiness = new AdminBusiness();
+        try{
+            return Response.ok(adminBusiness.viewPendingGymRequests()).build();
+        }catch(Exception exception){
+            return Response.status(Response.Status.UNAUTHORIZED).entity(exception.getMessage()).build();
+        }
+    }
+    @Path("allPendingGymOwnerRequests")
+    @GET
+    @Produces("application/json")
+    public static Response viewPendingGymOwnerRequests() {
+        AdminBusinessInterface adminBusiness = new AdminBusiness();
+        try{
+            return Response.ok(adminBusiness.viewPendingGymOwnerRequests()).build();
+        }catch(Exception exception){
+            return Response.status(Response.Status.UNAUTHORIZED).entity(exception.getMessage()).build();
+        }
+    }
+
+    @Path("allApprovedGyms")
+    @GET
+    @Produces("application/json")
+    public static Response viewAllApprovedGyms() {
+        AdminBusinessInterface adminBusiness = new AdminBusiness();
+        try{
+            return Response.ok(adminBusiness.viewAllApprovedGyms()).build();
+        }catch(Exception exception){
+            return Response.status(Response.Status.UNAUTHORIZED).entity(exception.getMessage()).build();
+        }
+    }
+
+    @Path("approveGym")
+    @PUT
+    public static String approveGymRegistration(@QueryParam("gymId") int gymId){
+
+        try{
+            AdminBusinessInterface adminBusiness = new AdminBusiness();
+            adminBusiness.approveGymRegistration(gymId);
+            return "Gym Approved!";
+        }catch(Exception exception){
+            return  exception.getMessage();
+        }
+    }
+
+    @Path("approveGymOwner")
+    @PUT
+    public static String approveGymOwnerRegistration(@QueryParam("ownerId") int ownerId){
+
+        try{
+            AdminBusinessInterface adminBusiness = new AdminBusiness();
+            adminBusiness.approveGymOwnerRegistration(ownerId);
+            return "GymOwner Approved!";
+        }catch(Exception exception){
+            return  exception.getMessage();
+        }
+    }
+
+
 }
