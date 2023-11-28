f2c4edf95986d05e765cfcfc318a9fb5101e985d
miti2711 <mitivarshney2711@gmail.com>
2023-11-29 01:40:02 +0530

    Implemented GymOwnerGMSRESTService

--- GMS_JEDI_PROJECT_REST_DROPWIZARD/GMSJEDIProjectRESTDropWizard/src/main/java/com/flipkart/rest/GymOwnerGMSRESTService.java
+++ GMS_JEDI_PROJECT_REST_DROPWIZARD/GMSJEDIProjectRESTDropWizard/src/main/java/com/flipkart/rest/GymOwnerGMSRESTService.java
@@ -28,8 +28,124 @@ public class GymOwnerGMSRESTService {
         }
 
     }
-//
-//    public static Response addSlots(int gymCenterId, Slot slot){
+    @Path("viewAllSlots")
+    @GET
+    @Produces("application/json")
+    public static Response viewAllSlots(@QueryParam("gymCenterId") int gymCenterId){
+        GymOwnerBusinessInterface gymOwnerBusiness = new GymOwnerBusiness();
+//        System.out.println("Listing all Slots in  GymCenter");
+//        System.out.println("-------------------------------------");
+        try{
+            return Response.ok(gymOwnerBusiness.viewAllSlots(gymCenterId)).build();
+        }
+        catch(Exception exception){
+            return Response.status(Response.Status.UNAUTHORIZED).entity(exception.getMessage()).build();
+        }
+    }
+    @Path("isApprovedGymOwner")
+    @GET
+    @Produces("application/json")
+    public static Response isApproved(@QueryParam("gymOwnerEmail") String gymOwnerEmail) {
+        GymOwnerBusinessInterface gymOwnerBusiness = new GymOwnerBusiness();
+        try{
+             return Response.ok().entity(gymOwnerBusiness.isApproved(gymOwnerEmail)).build();
+        }
+        catch(Exception exception){
+            return Response.status(Response.Status.UNAUTHORIZED).entity(exception.getMessage()).build();
+        }
+    }
+    @Path("isApprovedGymCenter")
+    @GET
+    @Produces("application/json")
+    public static Response isApproved(@QueryParam("gymCenterId") int gymCenterId)
+    {
+        GymOwnerBusinessInterface gymOwnerBusiness = new GymOwnerBusiness();
+        try
+        {
+            return Response.ok().entity(gymOwnerBusiness.isApproved(gymCenterId)).build();
+        }
+        catch(Exception exception){
+            return Response.status(Response.Status.UNAUTHORIZED).entity(exception.getMessage()).build();
+        }
+
+    }
+//    @Path("addSlot")
+//    @POST
+//    @Produces("application/json")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public static Response addSlots(@QueryParam("gymCenterId") int gymCenterId,Slot slot) {
+//        GymOwnerBusinessInterface gymOwnerBusiness = new GymOwnerBusiness();
+//        try
+//        {
+//             gymOwnerBusiness.addSlots(gymCenterId, slot);
+//            return Response.ok().entity("Slot added successfully").build();
+//        }
+//        catch(Exception exception){
+//            return Response.status(Response.Status.UNAUTHORIZED).entity(exception.getMessage()).build();
+//        }
 //
 //    }
+    @Path("addGym")
+    @POST
+    @Produces("application/json")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public static Response addGym(GymCenter gymCenter) {
+        GymOwnerBusinessInterface gymOwnerBusiness = new GymOwnerBusiness();
+        try
+        {
+            gymOwnerBusiness.addGym(gymCenter);
+            return Response.ok().entity("GymCenter added successfully").build();
+        }
+        catch(Exception exception){
+            return Response.status(Response.Status.UNAUTHORIZED).entity(exception.getMessage()).build();
+        }
+
+    }
+    @Path("createSlot")
+    @POST
+    @Produces("application/json")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public static Response createSlot(Slot slot){
+
+        GymOwnerBusinessInterface gymOwnerBusiness = new GymOwnerBusiness();
+        try
+        {
+            gymOwnerBusiness.createSlot(slot);
+            return Response.ok().entity("Slot added successfully").build();
+        }
+        catch(Exception exception){
+            return Response.status(Response.Status.UNAUTHORIZED).entity(exception.getMessage()).build();
+        }
+
+
+    }
+
+    @Path("viewAllGymCenters")
+    @GET
+    @Produces("application/json")
+    public static Response viewAllGymCenters(@QueryParam("gymOwnerEmail") String gymOwnerEmail){
+        GymOwnerBusinessInterface gymOwnerBusiness = new GymOwnerBusiness();
+//        System.out.println("Listing all GymCenter");
+//        System.out.println("-------------------------------------");
+        try{
+            return Response.ok(gymOwnerBusiness.viewAllGymCenters(gymOwnerEmail)).build();
+        } catch(Exception exception){
+            return Response.status(Response.Status.UNAUTHORIZED).entity(exception.getMessage()).build();
+        }
+    }
+    @Path("viewAllApprovedGymCenters")
+    @GET
+    @Produces("application/json")
+    public static Response viewAllApprovedGymCenters(@QueryParam("gymOwnerEmail") String gymOwnerEmail){
+//        System.out.println("Listing all approved GymCenter");
+//        System.out.println("-------------------------------------");
+        try{
+            return Response.ok(gymOwnerDao.viewAllApprovedGymCenters(gymOwnerEmail)).build();
+        }
+        catch(Exception exception){
+            return Response.status(Response.Status.UNAUTHORIZED).entity(exception.getMessage()).build();
+        }
+
+    }
+
 }
